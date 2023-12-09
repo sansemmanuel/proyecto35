6344ae406b9c2914618ae988faee872ce4aa64bf
Emmanuel Sansberro <36830673+sansemmanuel@users.noreply.github.com>
2023-12-09 01:29:55 -0300

    act

--- client/app/constants/index.js
+++ client/app/constants/index.js
@@ -1,9 +1,6 @@
 export const API_URL = process.env.API_URL;
 
-export const SOCKET_URL =
-  window.location.host.indexOf('localhost') >= 0
-    ? 'https://127.0.0.1:3000'
-    : window.location.host;
+export const SOCKET_URL = API_URL;
 
 export const ROLES = {
   Admin: 'ROLE ADMIN',
